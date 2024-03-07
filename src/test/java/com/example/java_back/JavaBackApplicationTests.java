63b68b612a58e0b86e26de936664e426b9f7b0ee
pialeonor12 <jorgeadrian_pucheta@hotmail.com>
2024-03-07 01:42:20 -0300

    primer push

---
+++ src/test/java/com/example/java_back/JavaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
